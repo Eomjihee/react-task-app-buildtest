6d4462bc41e8b9f0db32705f5f4866dfeb078131
Eomjihee <eomjihee.code@gmail.com>
2024-05-28 18:46:30 +0900

    predeploy 설정 및 firebase.ts 추가

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGW14QR7EWfX9CzROkViDa36x6zBBlDws",
+  authDomain: "react-task-app-9a2f2.firebaseapp.com",
+  projectId: "react-task-app-9a2f2",
+  storageBucket: "react-task-app-9a2f2.appspot.com",
+  messagingSenderId: "608407391364",
+  appId: "1:608407391364:web:b304c80b6207e14a36c6d1",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
